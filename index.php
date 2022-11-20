8e2136c3b89510977792bb4def2b6c4144877ab9
VincentDeriu <13084057+VincentDeriu@users.noreply.github.com>
2022-11-20 08:16:27 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+	<!--<link rel="stylesheet" type="text/css" href="style.css"> -->
+	<!--<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">-->
+</head>
+<body>
+	<div>
+		<div>
+			<div>
+				<h1>Hello World</h1>
+			</div>
+		</div>
+	</div>	
+</body>
+</html>
+
